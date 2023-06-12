21b6c34e569000d71646493c67b028e899f57d98
PauloGN <paulorsantos.gnt@outlook.com>
2023-06-12 01:22:34 -0700

    [46 Pickup Widget]

--- Source/Shooter/Weapon/Weapon.cpp
+++ Source/Shooter/Weapon/Weapon.cpp
@@ -3,34 +3,56 @@
 
 #include "Weapon.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Shooter/Character/MainCharacter.h"
 
 AWeapon::AWeapon()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	SetReplicates(true);
+	bReplicates = true;
 
 	weaponMesh = CreateAbstractDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
-	weaponMesh->SetupAttachment(RootComponent);
 	SetRootComponent(weaponMesh);
 
 	weaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
 	weaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
-	weaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 	areaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreSphere"));
 	areaSphere->SetupAttachment(RootComponent);
 	areaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	areaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	pickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	pickupWidget->SetupAttachment(RootComponent);
+
 }
 
 void AWeapon::BeginPlay()
 {
+
 	Super::BeginPlay();
-	
-	if (GetLocalRole() == ENetRole::ROLE_Authority)
+
+	if (HasAuthority())
 	{
 		areaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		areaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+		areaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	}
+
+	if (pickupWidget)
+	{
+		pickupWidget->SetVisibility(false);
+	}
+}
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+	AMainCharacter* MainChar = Cast<AMainCharacter>(OtherActor);
+
+	if (MainChar && pickupWidget)
+	{
+		pickupWidget->SetVisibility(true);
 	}
 }
 
--- Source/Shooter/Weapon/Weapon.h
+++ Source/Shooter/Weapon/Weapon.h
@@ -26,7 +26,12 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 protected:
+
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 
 private:
 
@@ -36,8 +41,11 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
 	class USphereComponent* areaSphere;
 
-	UPROPERTY(visibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
 	EWeaponState weaponState;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	class UWidgetComponent* pickupWidget;
 
 public:	
 
